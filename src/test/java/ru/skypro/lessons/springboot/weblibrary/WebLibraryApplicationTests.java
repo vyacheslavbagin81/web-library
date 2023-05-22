c16024c053dfdc59ec1d72f21e05c7f1775d891b
vyacheslavbagin@gmail.com <vyacheslavbagin@gmail.com>
2023-05-22 17:59:54 +0300

    web-library

---
+++ src/test/java/ru/skypro/lessons/springboot/weblibrary/WebLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.weblibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
